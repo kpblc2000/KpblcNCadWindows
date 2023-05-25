c7c0926c8b1001f95aeb07618487826df7c1d719
kpblc2000 <kpblc2000@gmail.com>
2023-05-25 08:45:41 +0300

    Extension + Link to ncad libraries

---
+++ KpblcNCadExtensions/Interfaces/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace KpblcNCadExtensions.Interfaces
+{
+    public interface IMessageService
+    {
+        void ErrorMessage(string Message, [CallerMemberName] string CallMethodName = null);
+        void InfoMessage(string Message, [CallerMemberName] string CallMethodName = null);
+        void ExceptionMessage(string Message, [CallerMemberName] string CallMethodName = null);
+        void ConsoleMessge(string Message, [CallerMemberName] string CallMethodName = null);
+
+    }
+}
