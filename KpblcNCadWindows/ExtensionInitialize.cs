5253728d3b6596b44f63a90a8db35bc83711d4ac
kpblc2000 <kpblc2000@gmail.com>
2023-05-25 09:37:05 +0300

    Готовый вариант (?)

---
+++ KpblcNCadWindows/ExtensionInitialize.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Teigha.Runtime;
+
+namespace KpblcNCadWindows
+{
+    public class ExtensionInitialize : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainAssemblyResolve;
+        }
+        
+        public void Terminate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Assembly? CurrentDomainAssemblyResolve(object? sender, ResolveEventArgs args)
+        {
+            string name = GetAssemblyName(args);
+            string path = Path.Combine(Path.GetDirectoryName(typeof(ExtensionInitialize).Assembly.Location),
+                name + ".dll");
+            if (File.Exists(path))
+            {
+                Assembly assembly = Assembly.LoadFrom(path);
+                if (assembly.FullName == args.Name)
+                {
+                    return assembly;
+                }
+            }
+
+            return null;
+        }
+
+        private string GetAssemblyName(ResolveEventArgs Args)
+        {
+            if (Args.Name.IndexOf(",") > -1)
+            {
+                return Args.Name.Substring(0, Args.Name.IndexOf(","));
+            }
+
+            return Args.Name;
+        }
+    }
+}
---
+++ KpblcNCadWindows/NCadCommands/WindowCommand.cs
@@ -0,0 +1,17 @@
+﻿using KpblcNCadWindows.Infrastructure;
+using KpblcNCadWindows.Views;
+using Teigha.Runtime;
+
+namespace KpblcNCadWindows.NCadCommands
+{
+    public static class WindowCommand
+    {
+        [CommandMethod("kpblc-win")]
+        public static void KpblcWindowCommand()
+        {
+            CadService cadService = new CadService();
+            EmptyWindow win = new EmptyWindow();
+            cadService.ShowModalWindow(win);
+        }
+    }
+}
